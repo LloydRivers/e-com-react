f185052e94f8d7ff4ffeb3a4cac29d70af3bc629
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-10 08:26:49 +0100

    add Link to header navbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -41,62 +42,19 @@ const Header = () => {
         <div className="container">
           <ul className="main-menu">
             <li>
-              <a href="#">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <a href="#">Checkout</a>
+              <Link to="/checkout">Checkout</Link>
             </li>
             <li>
-              <a href="#">Cart</a>
+              <Link to="/cart">Cart</Link>
             </li>
             <li>
               <a href="#">
                 Jewelry
                 <span className="new">New</span>
               </a>
-            </li>
-            {/* <li>
-              <a href="#">Shoes</a>
-              <ul className="sub-menu">
-                <li>
-                  <a href="#">Sneakers</a>
-                </li>
-                <li>
-                  <a href="#">Sandals</a>
-                </li>
-                <li>
-                  <a href="#">Formal Shoes</a>
-                </li>
-                <li>
-                  <a href="#">Boots</a>
-                </li>
-                <li>
-                  <a href="#">Flip Flops</a>
-                </li>
-              </ul>
-            </li> */}
-            {/* <li>
-              <a href="#">Pages</a>
-              <ul className="sub-menu">
-                <li>
-                  <a href="./product.html">Product Page</a>
-                </li>
-                <li>
-                  <a href="./category.html">Category Page</a>
-                </li>
-                <li>
-                  <a href="./cart.html">Cart Page</a>
-                </li>
-                <li>
-                  <a href="./checkout.html">Checkout Page</a>
-                </li>
-                <li>
-                  <a href="./contact.html">Contact Page</a>
-                </li>
-              </ul>
-            </li> */}
-            <li>
-              <a href="#">Blog</a>
             </li>
           </ul>
         </div>
