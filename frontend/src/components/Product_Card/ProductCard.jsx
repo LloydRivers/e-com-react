0098e5da7be8c2b52f08f2c2256382c49a8b8a9c
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-16 10:04:50 +0100

    add sweet alert

--- frontend/src/components/Product_Card/ProductCard.jsx
+++ frontend/src/components/Product_Card/ProductCard.jsx
@@ -4,10 +4,22 @@ import { addProduct } from "../../Redux/slices/ProductSlices/productSlice";
 import { addToCart } from "../../Redux/slices/CartSlices/cartSlice";
 import { useDispatch } from "react-redux";
 
+import Swal from "sweetalert2";
+
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
   const { brandName, imageUrl, id, price } = product;
-  console.log(product);
+
+  const addedToCartConformation = () => {
+    dispatch(addToCart(product));
+    Swal.fire({
+      title: "Added to cart",
+      text: "Product has been added to cart",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
 
   return (
     <div className="col-lg-3 col-sm-6">
@@ -15,10 +27,7 @@ const ProductCard = ({ product }) => {
         <div className="pi-pic">
           <img src={`https://${imageUrl}`} alt="product" />
           <div className="pi-links">
-            <a
-              className="add-card"
-              onClick={() => dispatch(addToCart(product))}
-            >
+            <a className="add-card" onClick={addedToCartConformation}>
               <i className="flaticon-bag"></i>
               <span>ADD TO CART</span>
             </a>
