e0c0e16662f626e2a4353dd6510a9083189ffabd
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-19 21:30:29 +0100

    add express

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+// create express app
+const express = require("express");
+const app = express();
+// Adding MassiveJS
+const massive = require("massive");
+require("dotenv").config();
+let { POSTGRES_URI, PORT } = process.env;
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const {
+  getProducts,
+  getSpecificProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} = require("./controllers/productController");
+
+massive(
+  {
+    connectionString: POSTGRES_URI,
+    ssl: { rejectUnauthorized: false },
+  },
+  {
+    scripts: "./db",
+  }
+)
+  .then((db) => {
+    app.set("db", db);
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("DB Connection Failed!");
+  });
+
+app.get("/getProducts", getProducts);
+app.get("/getSpecificProduct/:id", getSpecificProduct);
+app.post("/createProduct", createProduct);
+app.delete("/deleteProduct/:id", deleteProduct);
+app.put("/updateProduct/:id", updateProduct);
+
+/****************************************/
+
+app.listen((PORT = 5000), () => {
+  console.log(`Server running on port ${PORT}`);
+});
