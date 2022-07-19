91e67af86dc0c0d5840bbe61432bda3e779cb76f
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-19 21:36:12 +0100

    setup index export/imports

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,5 +3,12 @@ import CartPage from "./Cart_Page/CartPage";
 import DetailsPage from "./Details_Page/DetailsPage";
 import CheckOutPage from "./Checkout_Page/CheckOutPage";
 import ContactPage from "./Contact_Page/ContactPage";
-
-export { HomePage, CartPage, DetailsPage, CheckOutPage, ContactPage };
+import AddProductForm from "./Admin/AddProductForm";
+export {
+  HomePage,
+  CartPage,
+  DetailsPage,
+  CheckOutPage,
+  ContactPage,
+  AddProductForm,
+};
