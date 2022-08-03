9ff8de38e9e07a17a3508e55d1d9afc17e48a223
Mouzey406 <muuo947@gmail.com>
2022-08-03 23:03:37 +0300

    Made some stylings

--- frontend/src/Redux/slices/ProductSlices/productsSlice.js
+++ frontend/src/Redux/slices/ProductSlices/productsSlice.js
@@ -37,7 +37,7 @@ const data = () => {
       .then((res) => res.data);
   } catch (error) {
     console.log(error);
-  }
+  } 
 };
 
 export const fetchProducts = createAsyncThunk("products/fetchProducts", data);
