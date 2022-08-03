893cc37afd0eae18fc658fdb28eea62742078d81
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-03 07:51:36 +0100

    import payment card

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,5 +4,14 @@ import Navbar from "./NavBar/Navbar";
 import ProductCard from "./Product_Card/ProductCard";
 import CartCard from "./Cart_Card/CartCard";
 import ProtectedRoute from "./Protected_Routes/ProtectedRoute";
+import PaymentCard from "./Payment_Card/PaymentCard";
 
-export { Footer, Header, Navbar, ProductCard, CartCard, ProtectedRoute };
+export {
+  Footer,
+  Header,
+  Navbar,
+  ProductCard,
+  CartCard,
+  ProtectedRoute,
+  PaymentCard,
+};
