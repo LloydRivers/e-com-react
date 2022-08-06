d883ed87897da23620655f245ba3a5a0d71d2008
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-06 12:37:07 +0100

    add bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap";
+
 import { Provider } from "react-redux";
 import { store } from "./Redux/app/store";
 import App from "./App";
